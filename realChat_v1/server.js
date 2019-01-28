ba3ea2a5e8df98d0dfbab23de28cef3ec00829f4
Yuseon Kang <39141532+emma-kang@users.noreply.github.com>
2019-01-28 07:17:23 -0500

    Update server.js

--- realChat_v1/server.js
+++ realChat_v1/server.js
@@ -227,8 +227,3 @@ io.on('connection', function(socket){
 server.listen(port, () => { // never app.listen
     console.log(`Express http server listening on ${port}`);
 });
-
-// CHANGE THE APP IN HEROKU
-// PS C:\Users\hongs\dev\js\server_side_javascript\RealChat> git remote rm heroku
-// PS C:\Users\hongs\dev\js\server_side_javascript\RealChat> heroku git:remote -a fast-spire-12846 (new app name)
-// fatal: 'heroku' does not appear to be a git repository
